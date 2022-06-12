a67eefeb7887e530237217cc208e3de0367f0235
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2022-06-12 23:58:06 +0530

    Update internet_control.py

--- gripper_bot_description/src/internet_control.py
+++ gripper_bot_description/src/internet_control.py
@@ -15,14 +15,16 @@ firebaseConfig= {
     "appId": "*************************************"
 }
 
-firebase=pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
-db=firebase.database()
+db = firebase.database()
 
 count = 0
 
-pubslide1 = rospy.Publisher('/gripper_bot/gripper_joint_position_controller/command',Float64,queue_size=1)
-pubslide2 = rospy.Publisher('/gripper_bot/arm_joint_position_controller/command',Float64,queue_size=1)
+pubslide1 = rospy.Publisher(
+    '/gripper_bot/gripper_joint_position_controller/command', Float64, queue_size=1)
+pubslide2 = rospy.Publisher(
+    '/gripper_bot/arm_joint_position_controller/command', Float64, queue_size=1)
 rospy.init_node('ROScontrol', anonymous=True)
 
 velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -35,30 +37,38 @@ while True:
         print(1)
         pubslide1.publish(0.01)
     elif arm_control == 1:
-        print(76)
-        pubslide1.publish(0.76)
+        print(19)
+        pubslide1.publish(0.19)
     elif arm_control == 2:
+        print(38)
+        pubslide1.publish(0.38)
+    elif arm_control == 3:
+        print(57)
+        pubslide1.publish(0.57)
+    elif arm_control == 4:
+        print(77)
+        pubslide1.publish(0.77)
+    elif arm_control == 5:
+        print(96)
+        pubslide1.publish(0.96)
+    elif arm_control == 6:
+        print(115)
+        pubslide1.publish(1.15)
+    elif arm_control == 7:
+        print(135)
+        pubslide1.publish(1.35)
+    elif arm_control == 8:
         print(157)
         pubslide1.publish(1.57)
     # print(count, users)
     # time.sleep(.5)
     # count = count + 1
-    
+
     direction = db.child("test").child("command").get().val()
 
     if direction == "FR":
         print("FRONT")
         vel_msg.linear.x = 0.5
-        vel_msg.linear.y = 0
-        vel_msg.linear.z = 0
-        vel_msg.angular.x = 0
-        vel_msg.angular.y = 0
-        vel_msg.angular.z = 0
-        velocity_publisher.publish(vel_msg)
-
-    elif direction == "BK":
-        print("BACK")
-        vel_msg.linear.x = -0.5
         vel_msg.linear.y = 0
         vel_msg.linear.z = 0
         vel_msg.angular.x = 0
@@ -115,3 +125,32 @@ while True:
         vel_msg.angular.y = 0
         vel_msg.angular.z = -1.0
         velocity_publisher.publish(vel_msg)
+
+    elif direction == "BK":
+        print("BACK")
+        vel_msg.linear.x = -0.5
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 0
+        velocity_publisher.publish(vel_msg)
+
+    elif direction == "ZIGZAG":
+        print("ZIGZAG")
+        vel_msg.linear.x = 0.5
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 3.0
+        velocity_publisher.publish(vel_msg)
+        time.sleep(2.5)
+        vel_msg.linear.x = 0.5
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = -3.0
+        velocity_publisher.publish(vel_msg)
+        time.sleep(2.5)
